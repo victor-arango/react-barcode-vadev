501411c6f0db317561022f9b4e417db5189bdac0
Victor Arango <victorarangoquiroz@gmail.com>
2024-11-12 12:56:55 -0500

    configuration

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: './src/index.jsx',
+  output: {
+    file: 'dist/index.es.js',
+    format: 'es',
+    exports: 'named',
+  },
+  plugins: [
+    resolve(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-react'],
+    }),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
+};
